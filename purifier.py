33990af8137834a4b6d2417d37dea2264dcceb8e
goodidea <goodidea@platanus.(none)>
2014-05-18 01:24:27 -0700

    squeeze and edit distance added by Zijun

--- purifier.py
+++ purifier.py
@@ -8,9 +8,50 @@
 # -> list of tuples (candidate, p) where p is relative probability
 
 
-import re, json
+import re, json, collections
 from operator import itemgetter
 import unicodedata
+
+class Mysqueezer:
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+    def find_duplicates(self,word):
+        start=-1
+        end = -1
+        for i in range(len(word)-2):
+            if word[i]==word[i+1] and word[i+1] == word[i+2]:
+                start = i
+                k = i+1
+                print( start,k)
+                while k<len(word) and word[k]==word[i]:
+                    k+=1
+                end = k-1
+                break
+        return (start,end)
+    
+    def squeeze(self,word):
+        lists = []
+        (start,end) = self.find_duplicates(word)
+        lists += self.gen(word,start,end)     
+        return lists
+    def gen(self,word,start,end):
+        ret = []
+        if start==-1:
+            ret.append(word)
+            return ret
+        list1 = word[0:start]+word[end:]
+        list2 = word[0:start+1]+word[end:]
+        (s,e) = self.find_duplicates(list1)
+        ret += self.gen(list1, s, e) 
+        (s,e) = self.find_duplicates(list2)
+        ret += self.gen(list2, s, e)
+        return ret
 
 def norm(s):
 	return unicodedata.normalize('NFKD', s).encode('ascii','ignore')
@@ -37,11 +78,61 @@ def cleanse(f, o):
 
 # TODO: fill me out
 def squeeze(word):
-	return [word]
+	squeezer = Mysqueezer()
+	return squeezer.squeeze(word)
 
 # TODO: fill me out with default distance 1/(1+n)
+
+def words(text): return re.findall('[a-z]+', text.lower()) 
+
+def train(features):
+    model = collections.defaultdict(lambda: 1)
+    for f in features:
+        model[f] += 1
+    return model
+
+NWORDS = train(words(file('big.txt').read()))
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def edits1(word):
+   splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+   deletes    = [a + b[1:] for a, b in splits if b]
+   transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
+   replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
+   inserts    = [a + c + b     for a, b in splits for c in alphabet]
+   return set(deletes + transposes + replaces + inserts)
+
+def known_edits2(word):
+    return set(e2 for e1 in edits1(word) for e2 in edits1(e1) if e2 in NWORDS)
+
+def known(words): return set(w for w in words if w in NWORDS)
+
+def correct(word):
+    candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
+#    if(known[word]) return (word,0)
+#    if(edits1(word)) return (word,
+    return max(candidates, key=NWORDS.get)
+def correct_new(word):
+    set0 = known([word])
+    set1 = known(known(edits1(word)))
+    set2 = known_edits2(word)
+
+    lst = []
+    tmp = list(set0)
+    for i in tmp:
+	lst  += (i,0)
+    tmp = list(set1)
+    for i in tmp:
+	lst +=(i,1)
+    tmp = list(set2)
+    for i in tmp:
+	lst +=(i,2)
+    return lst	
+
 def edit_candidates(word, d):
-	return [(word, 1)]
+		
+	return correct_new(word)
 
 # TODO: fill me out with distance 0.5
 def phonetic_candidates(word):
@@ -94,4 +185,5 @@ dict_freq = open("wordFreqDict.json")
 dict_freq = json.load(dict_freq)
 dict_abbrword = open("abbrev_word.json")
 dict_abbrword = json.load(dict_abbrword)
-
+print(squeeze("goooood"))
+print(correct_new('spelling'))
