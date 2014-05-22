5c5184b682d5db0e897d3fb7a8158c2ada70ef34
Jennifer Zhang <jxzhang@post.harvard.edu>
2014-05-22 15:24:11 -0700

    Adding a tentative viterbi matching

--- purifier.py
+++ purifier.py
@@ -8,7 +8,7 @@
 # -> list of tuples (candidate, p) where p is relative probability
 
 
-import re, json, collections
+import re, json, collections, numpy, math
 from operator import itemgetter
 import unicodedata
 
@@ -91,10 +91,6 @@ def train(features):
         model[f] += 1
     return model
 
-NWORDS = train(words(file('big.txt').read()))
-
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
 def edits1(word):
    splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
    deletes    = [a + b[1:] for a, b in splits if b]
@@ -134,15 +130,16 @@ def edit_candidates(word, d):
 		
 	return correct_new(word)
 
-# TODO: fill me out with distance 0.5
-def phonetic_candidates(word):
+# Returns phonetic candidates (list of tuples)
+# TODO: Check whether 0.5 is realistic
+def phonetic_candidates(word, d):
 	phonetic_representation=dict_soundex[word]
 	phonetic_representation=phonetic_representation[0]
 	print(phonetic_representation)
 	word_list =dict_inverted_soundex[phonetic_representation]
 	phonetic_candidates=[]
-	for word in word_list:
-		phonetic_candidates.append((word, 0.5))
+	for w in word_list:
+		phonetic_candidates.append((w, 0.5*d*letter_sim(word, w)))
 		
 	return phonetic_candidates
 
@@ -172,13 +169,89 @@ def word_freq(word):
 	else:
 		return dict_freq[word]
 
+# Ranking the phonetic candidates by letter commonality
+# Consonants worth more than vowels
+# Returns the tracking value and the best viterbi value for global alignment
+def read_scoring(scorefile):
+    state = 0
+    header = []
+    sm = {}
+    for line in open(scorefile, 'r'):
+        line = line.rstrip('\n')
+        line = line.rstrip('\r')
+        if None == re.search('^#', line):
+            a = line.split(' ')
+            if 0 == state:
+                header = a
+                state = state + 1
+            else:
+                if len(a) != 1 + len(header):
+                    print('error: the number of entries did not match the header')
+                    sys.exit(2)
+                for i in range(len(header)):
+                    sm[a[0] + header[i]] = float(a[i+1])
+    
+    return sm
+
+def bestglobal(m, sm, r, c, s1, s2):
+	if (s1 not in alphabet):
+		s1 = '*'
+	if (s2 not in alphabet):
+		s2 = '*'
+
+	diag = m[r-1, c-1] + sm[s1 + s2]
+	across = m[r, c-1] + gpenalty
+	down = m[r-1, c] + gpenalty
+	best = max(diag, across, down)
+
+	if (best == down):
+		t = -1
+	elif (best == across):
+		t = 1
+	elif (best == diag):
+		t = 0
+
+	return (t, best)
+
+def align(a, b, sm):
+    n = len(a) + 1 # rows
+    m = len(b) + 1 # cols
+	
+    gv = numpy.zeros(n * m).reshape(n, m) # global viterbi matrix
+    gvtrack = numpy.zeros(n * m).reshape(n, m) # tracking for gv
+    
+    gvtrack[0,0] = '-999' # represents a start point in string alignment
+    
+    # initialize the gv matrix
+    for i in range(0, m):
+        gv[0, i] = i * gpenalty
+        if (i != 0):
+            gvtrack[0, i] = 1
+    for i in range (0, n):
+        gv[i, 0] = i * gpenalty
+        if (i != 0):
+            gvtrack[i, 0] = -1
+	
+    for r in range(1, n):
+        for c in range (1, m):
+            (t, v) = bestglobal(gv, sm, r, c, b[c-1], a[r-1])
+            gv[r, c] = v
+            gvtrack[r, c] = t
+    return gv[n-1, m-1]
+
+def letter_sim(word, candidate):
+	score = align(word, word, dict_letters)
+	sim = align(word, candidate, dict_letters)
+	return sim/score
+
 # Returns a list of candidate word tuples in descending probability order
 def word_correct(word):
 	words = squeeze(word)
 	candidates = []
 	for w in words:
-		candidates += edit_candidates(w, 1)
-		candidates += phonetic_candidates(w)
+		# TODO: uncomment me when edit_candidates is fixed
+		#candidates += edit_candidates(w, 1)
+		candidates += phonetic_candidates(w, 1)
 
 	candidates = compress(candidates)
 	c = []
@@ -190,6 +263,9 @@ def word_correct(word):
 	print sorted(c, key=itemgetter(1), reverse=True)
 
 # temporary globals: loading dictionaries
+NWORDS = train(words(file('big.txt').read()))
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
 dict_freq = open("wordFreqDict.json")
 dict_freq = json.load(dict_freq)
 dict_abbrword = open("abbrev_word.json")
@@ -198,4 +274,6 @@ dict_soundex=open("soundexDict_hashTable.json")
 dict_soundex=json.load(dict_soundex)
 dict_inverted_soundex=open("inverted_soundexDict.json")
 dict_inverted_soundex=json.load(dict_inverted_soundex)
+dict_letters = read_scoring("letter_scoring.txt")
+gpenalty = 0
 
