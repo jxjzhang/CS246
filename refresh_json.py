b1f24e05ff98e27ef1ae0af45384cf71e92f7916
Jennifer Zhang <jxzhang@post.harvard.edu>
2014-05-24 14:33:35 -0700

    Converting NWORDS to a json dictionary Renaming abbrev.py to refresh_json.py

--- refresh_json.py
+++ refresh_json.py
@@ -16,6 +16,18 @@ def make_dict(f, output):
 	with open(output, 'w') as out:
 		json.dump(d, out)
 
+def make_json(f, output):
+	input = open(f, "r")
+	d = {}
+	for line in input:
+		for word in line.split():
+			word = word.lower()
+			if not word in d:
+				d[word] = 1
+	with open(output, 'w') as out:
+		json.dump(d, out)
+
 make_dict('abbrev_word.txt','abbrev_word.json')
 make_dict('abbrev_phrase.txt','abbrev_phrase.json')
 make_dict('abbrev_sem.txt','abbrev_sem.json')
+make_json('ispell_dict.txt', 'dict.json')
